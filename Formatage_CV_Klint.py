b4613c8132ae53a86e319d948d6b832e36276c1c
Leeroyklint <lmoothery@klint-consulting.com>
2025-02-28 11:32:25 +0100

    Update Formatage_CV_Klint.py

--- Formatage_CV_Klint.py
+++ Formatage_CV_Klint.py
@@ -1,15 +1,9 @@
 import streamlit as st
-from service_cv import page_formatage_cv
 from service_chat import page_chat
 
 def main():
-    st.sidebar.title("Menu de navigation")
-    choix = st.sidebar.radio("Choisissez un service :", ("Formatage de CV", "Chat Conversationnel"))
-
-    if choix == "Formatage de CV":
-        page_formatage_cv()
-    elif choix == "Chat Conversationnel":
-        page_chat()
+    # Appel direct de la page de chat sans menu de navigation
+    page_chat()
 
 if __name__ == "__main__":
     main()
